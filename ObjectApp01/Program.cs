af21d293580e233a7b4c8bd93b0c304ebdb8f82b
z003sw8x <z003sw8x@DINMCND2252FWR>
2024-10-17 07:11:24 +0200

    Add project files.

---
+++ ObjectApp01/Program.cs
@@ -0,0 +1,47 @@
+﻿using System.Security.Cryptography.X509Certificates;
+using System.Speech;
+using System.Speech.Synthesis; //To output voice
+//using System.Speech.Recognition; //To recognize voice commands
+
+namespace ObjectApp01
+{
+    internal class Program
+    {   
+       
+
+        static void Main(string[] args)
+        {
+            SpeechSynthesizer synthesizer = new SpeechSynthesizer();
+            synthesizer.SetOutputToDefaultAudioDevice();
+            int maxNamesCount = 10;
+            Zdravic[] zdravicArray = new Zdravic[maxNamesCount];
+            Surename n = new Surename();
+            Surename[] surenames = n.GetSurnameArray(null);
+            int aa = surenames.Length;
+            if (aa > 0)
+            {
+                Console.WriteLine(aa);
+            }
+            for (int i = 0; i < maxNamesCount; i++)
+            {
+                zdravicArray[i] = new Zdravic();
+            }
+            Random rn = new Random();
+            
+            foreach (Zdravic z in zdravicArray)
+            {
+                string name1 = surenames[rn.Next(aa)].Name;
+                z.Pozdrav(name1);
+                synthesizer.Speak("Hello " + name1);
+            }
+            Console.ReadKey();
+        }
+    }
+    public class Zdravic
+    {
+        public void Pozdrav(string jmeno)
+        {
+            Console.WriteLine("Hello {0} Object World!", jmeno);
+        }
+    }
+}
---
+++ ObjectApp01/Surname.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectApp01
+{
+    internal class Surename
+    {
+        public Surename() {
+            this.Name = null;
+            this.Rank = 0;
+            this.Count = 0;
+            
+        }
+        public Surename(string name, int rank, int count) { 
+            this.Name = name;
+            this.Rank = rank;
+            this.Count = count;
+            
+        }
+
+        public string? Name { get; set; }
+        public int Rank { get; set; }
+        public int Count { get; set; }
+
+        
+
+        public Surename[] GetSurnameArray(string? pathToFile)
+        {
+            if (pathToFile == null || pathToFile.Length == 0)
+            {
+                pathToFile = "..\\..\\..\\Resources\\surnames.csv";
+                var lines = File.ReadLines(pathToFile).ToArray();
+                int count = lines.Count()-1;
+                Surename[] names = new Surename[count];
+                for (int i = 1; i < count; i++)
+                {
+                    string[] splitedLine = lines[i].Split(",");
+                    if (splitedLine.Length > 0)
+                    {
+                        names[i-1] = new Surename(splitedLine[0], int.Parse(splitedLine[1]), int.Parse(splitedLine[2]));
+                    }
+                }
+                return names;
+
+            }
+            return null;
+        }
+
+    }
+}
